ca74ca4cb9e5d5b0ced2ac0b673ec24b5b9153f4
George Kettle <georgekettle.gk@gmail.com>
2020-12-26 12:11:48 +1100

    posts and boards main functionality

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,64 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+
+    if params[:post][:photo]
+      cloudinary_obj = Cloudinary::Uploader.upload(params[:post][:photo])
+      @photo = Photo.new(url: cloudinary_obj["secure_url"])
+      @post.photo = @photo
+    end
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, success: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.js
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+        format.js
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.js
+      end
+    end
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to profile_url(current_user), destroyed: 'post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:title, :description, :board_id)
+  end
+end
